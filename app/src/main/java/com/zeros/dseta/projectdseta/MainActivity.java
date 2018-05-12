05f2b8a551a877b3da5351f1c5063064221aadaf
zeros <erwin.plaza.x@gmail.com>
2018-05-12 01:27:24 -0400

    Ultimo cambio para asegurar la integracion por linea de comandos

--- app/src/main/java/com/zeros/dseta/projectdseta/MainActivity.java
+++ app/src/main/java/com/zeros/dseta/projectdseta/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Nuevos cambios
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
